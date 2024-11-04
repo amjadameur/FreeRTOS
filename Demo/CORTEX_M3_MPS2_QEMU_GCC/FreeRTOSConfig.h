0390aa60762d75f60eff661a26d5a4905bdb1696
Amjad Ouled-Ameur <ouledameur.amjad@gmail.com>
2024-11-04 13:56:34 +0000

    Customise FreeRTOSConfig.h
    Signed-off-by: Amjad Ouled-Ameur <ouledameur.amjad@gmail.com>

--- Demo/CORTEX_M3_MPS2_QEMU_GCC/FreeRTOSConfig.h
+++ Demo/CORTEX_M3_MPS2_QEMU_GCC/FreeRTOSConfig.h
@@ -53,39 +53,50 @@ header file. */
 #define configMINIMAL_STACK_SIZE  ( ( unsigned short ) 200 )
 #define configTOTAL_HEAP_SIZE     ( ( size_t ) ( 279000 ) )
 #define configMAX_TASK_NAME_LEN   ( 10 )
-#define configUSE_TRACE_FACILITY    0
+#define configUSE_TRACE_FACILITY    1
 #define configUSE_16_BIT_TICKS      0
-#define configIDLE_SHOULD_YIELD     0
+#define configIDLE_SHOULD_YIELD     1
 #define configUSE_CO_ROUTINES       0
 
-#define configMAX_PRIORITIES            ( 10 )
+#define configMAX_PRIORITIES            ( 5 )
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
-#define configTIMER_QUEUE_LENGTH          20
-#define configTIMER_TASK_PRIORITY       ( configMAX_PRIORITIES - 3 )
+#define configTIMER_QUEUE_LENGTH          10
+#define configTIMER_TASK_PRIORITY       ( 2 )
 #define configUSE_COUNTING_SEMAPHORES 1
-#define configSUPPORT_DYNAMIC_ALLOCATION 1
-#define  configNUM_TX_DESCRIPTORS 15
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define configUSE_MALLOC_FAILED_HOOK    1
+#define configUSE_MALLOC_FAILED_HOOK    0
 #define configUSE_MUTEXES               1
 #define configUSE_RECURSIVE_MUTEXES     1
-#define INCLUDE_vTaskPrioritySet        0
-#define INCLUDE_uxTaskPriorityGet       0
-#define INCLUDE_vTaskDelete             0
-#define INCLUDE_vTaskCleanUpResources   0
-#define INCLUDE_vTaskSuspend            0
-#define INCLUDE_vTaskDelayUntil         1
-#define INCLUDE_vTaskDelay              1
+#define INCLUDE_vTaskPrioritySet		1
+#define INCLUDE_uxTaskPriorityGet		1
+#define INCLUDE_vTaskDelete				1
+#define INCLUDE_vTaskCleanUpResources	1
+#define INCLUDE_vTaskSuspend			1
+#define INCLUDE_vTaskDelayUntil			1
+#define INCLUDE_vTaskDelay				1
 
+#define PRIO_BITS       		3        /* 7 priority levels */
 
-#define configKERNEL_INTERRUPT_PRIORITY         255
+/* The lowest interrupt priority that can be used in a call to a "set priority"
+function. */
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY			0xf
+
+/* The highest interrupt priority that can be used by any interrupt service
+routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
+INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
+PRIORITY THAN THIS! (higher priorities are lower numeric values. */
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	5
+
+/* Interrupt priorities used by the kernel port layer itself.  These are generic
+to all Cortex-M ports, and do not rely on any particular library functions. */
+#define configKERNEL_INTERRUPT_PRIORITY 		( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - PRIO_BITS) )
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY    191 /* equivalent to 0xa0, or priority 5. */
-#define configMAC_INTERRUPT_PRIORITY 5
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - PRIO_BITS) )
+
 /* Prototype for the function used to print out.  In this case it prints to the
 |     10 console before the network is connected then a UDP port after the network has
 |      9 connected. */
